b5651861258adb9de82715b151c88f722609c927
hameed <sdhameed03@gmail.com>
2022-08-29 16:41:19 +0530

    commit v0.2

---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Nav.css";
+function Nav() {
+  return (
+    <div className="nav">
+      <h1 className="nav__head">NETFLIX</h1>
+    </div>
+  );
+}
+
+export default Nav;
